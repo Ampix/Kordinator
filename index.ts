1b8ac3b5ca1daf4d1e876428b7b6e0f5e78572d4
HVCsano <hvcsano@proton.me>
2023-12-23 19:27:37 +0100

    alapok

--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
-import express from 'express'
+let active: {name?:string,route?:string,next?:number,status?:"station"|"going",dest?:boolean}[] = []
 
-const app = express()
-const port = 8080
-
-app.get("/", (req,res) => {
-    res.sendFile(__dirname + "/pages/index.html")
-})
-
-app.listen(port,() => {
-    console.log("http://localhost:" + port);
+Bun.serve({
+    port: 3000,
+    development: true,
+    async fetch(req) {
+        const url = new URL(req.url)
+        if(url.pathname === "/style"){
+            return new Response(Bun.file("public/output.css"))
+        }else if(url.pathname === "/"){
+            return new Response(Bun.file("src/index.html"))
+        }else{
+            return new Response(Bun.file("src/404.html"))
+        }
+        
+    },
+    websocket: {
+        message(ws, message) {
+            
+        },
+    }
 })
